09ed022d2d7d33395dedfff0ec8e552dd22c0fca
bhandarisiddharth256 <bhandarisiddharth256@gmail.com>
2026-03-20 00:56:10 +0530

    Setup basic Express server

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+import restaurantRoutes from "./routes/restaurantRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/restaurants", restaurantRoutes);
+app.use("/orders", orderRoutes);
+
+export default app;
